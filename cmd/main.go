0650ef5498532ea8051397978124275f9ee66053
Scott Walker <scott.walker@puppet.com>
2020-03-20 16:41:06 +0000

    PuppetDB error handling fix

--- cmd/main.go
+++ cmd/main.go
@@ -10,11 +10,12 @@ import (
 
 func main() {
 
-	pdbHost := "https://lenient-veranda.delivery.puppetlabs.net:8081"
-	orchHost := "https://lenient-veranda.delivery.puppetlabs.net:8143"
+	peHost := "lenient-veranda.delivery.puppetlabs.net"
+	pdbHostURL := fmt.Sprintf("https://%s:8081", peHost)
+	orchHostURL := fmt.Sprintf("https://%s:8143", peHost)
 	token := "xxxx"
 
-	pdbClient := puppetdb.NewInsecureClient(pdbHost, token)
+	pdbClient := puppetdb.NewInsecureClient(pdbHostURL, token)
 	nodes, err := pdbClient.Nodes("")
 	if err != nil {
 		panic(err)
@@ -22,14 +23,14 @@ func main() {
 	spew.Dump(nodes)
 	fmt.Println()
 
-	nodes, err = pdbClient.Nodes(`["=", "certname", "lenient-veranda.delivery.puppetlabs.net"]`)
+	nodes, err = pdbClient.Nodes(fmt.Sprintf(`["=", "certname", "%s"]`, peHost))
 	if err != nil {
 		panic(err)
 	}
 	spew.Dump(nodes)
 	fmt.Println()
 
-	orchClient := orch.NewInsecureClient(orchHost, token)
+	orchClient := orch.NewInsecureClient(orchHostURL, token)
 	inv, err := orchClient.Inventory()
 	if err != nil {
 		panic(err)
