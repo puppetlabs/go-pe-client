d1d79c67276cbd3b8adaa64c27fee1bfee261627
Dave Johnston <dave.johnston@puppet.com>
2020-03-30 10:27:00 +0100

    Add Inventory Support
    This adds the ability to query the pdb inventory endpoint.

--- puppetdb/facts_test.go
+++ puppetdb/facts_test.go
@@ -9,7 +9,7 @@ import (
 // TestFactNames performs a test on the FactNames endpoint and verifies the expected response is returned.
 func TestFactNames(t *testing.T) {
 	// Test FactNames
-	setupGetResponder(t, "/pdb/query/v4/fact-names", "", "factnames-response.json")
+	setupGetResponder(t, factnames, "", "factnames-response.json")
 	actual, err := pdbClient.FactNames()
 	require.Nil(t, err)
 	require.Equal(t, expectedFactNames, actual)
@@ -18,8 +18,8 @@ func TestFactNames(t *testing.T) {
 // TestFacts performs a test on the Facts method, and verified the expected response is returned,
 func TestFacts(t *testing.T) {
 	// Test with query
-	query := `["=", "certname", "lenient-veranda.delivery.puppetlabs.net"]`
-	setupGetResponder(t, "/pdb/query/v4/facts", "query="+query, "facts-response.json")
+	query := `["=", "certname", "foobar.puppetlabs.net"]`
+	setupGetResponder(t, facts, "query="+query, "facts-response.json")
 	actual, err := pdbClient.Facts(query)
 	require.Nil(t, err)
 	require.Equal(t, expectedFacts, actual)
@@ -27,7 +27,7 @@ func TestFacts(t *testing.T) {
 
 var expectedFactNames = &[]string{"agent_canary", "agent_specified_environment", "aio_agent_build", "aio_agent_version"}
 var expectedFacts = &[]Fact{
-	{Name: "id", Value: "root", Certname: "jenkins-compose-worker-prod-2.delivery.puppetlabs.net", Environment: "production"},
-	{Name: "os", Value: map[string]interface{}{"architecture": "x86_64", "distro": map[string]interface{}{"codename": "Core", "description": "CentOS Linux release 7.4.1708 (Core)", "id": "CentOS", "release": map[string]interface{}{"full": "7.4.1708", "major": "7", "minor": "4"}, "specification": ":core-4.1-amd64:core-4.1-noarch"}, "family": "RedHat", "hardware": "x86_64", "name": "CentOS", "release": map[string]interface{}{"full": "7.4.1708", "major": "7", "minor": "4"}, "selinux": map[string]interface{}{"config_mode": "permissive", "config_policy": "targeted", "current_mode": "permissive", "enabled": true, "enforced": false, "policy_version": "28"}}, Certname: "jenkins-compose-worker-prod-2.delivery.puppetlabs.net", Environment: "production"},
-	{Name: "gid", Value: "root", Certname: "jenkins-compose-worker-prod-2.delivery.puppetlabs.net", Environment: "production"},
+	{Name: "id", Value: "root", Certname: "foobar.puppetlabs.net", Environment: "production"},
+	{Name: "os", Value: map[string]interface{}{"architecture": "x86_64", "distro": map[string]interface{}{"codename": "Core", "description": "CentOS Linux release 7.4.1708 (Core)", "id": "CentOS", "release": map[string]interface{}{"full": "7.4.1708", "major": "7", "minor": "4"}, "specification": ":core-4.1-amd64:core-4.1-noarch"}, "family": "RedHat", "hardware": "x86_64", "name": "CentOS", "release": map[string]interface{}{"full": "7.4.1708", "major": "7", "minor": "4"}, "selinux": map[string]interface{}{"config_mode": "permissive", "config_policy": "targeted", "current_mode": "permissive", "enabled": true, "enforced": false, "policy_version": "28"}}, Certname: "foobar.puppetlabs.net", Environment: "production"},
+	{Name: "gid", Value: "root", Certname: "foobar.puppetlabs.net", Environment: "production"},
 }
