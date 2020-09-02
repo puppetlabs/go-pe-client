de20b8735fa022618abda3af24eb640e96c7e8d0
Scott Walker <scott.walker@puppet.com>
2020-09-02 14:24:40 +0100

    Initial support for PE root API
    Add the root PE API i.e. https://mype.puppetlabs.net/api/...
    
    Right now only environments are supported.

---
+++ pkg/pe/client.go
@@ -0,0 +1,34 @@
+package pe
+
+import (
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+
+	"github.com/go-resty/resty/v2"
+)
+
+// Client for the PE API
+type Client struct {
+	resty  *resty.Client
+	strict bool
+}
+
+// NewClient access the PE API via TLS
+func NewClient(hostURL string, token string, tlsConfig *tls.Config) *Client {
+	r := resty.New()
+	if tlsConfig != nil {
+		r.SetTLSClientConfig(tlsConfig)
+	}
+	r.SetHostURL(hostURL)
+	r.SetHeader("X-Authentication", token)
+	client := Client{resty: r}
+	r.JSONUnmarshal = func(data []byte, v interface{}) error {
+		d := json.NewDecoder(bytes.NewReader(data))
+		if client.strict {
+			d.DisallowUnknownFields()
+		}
+		return d.Decode(v)
+	}
+	return &client
+}
