36dd1f5d83d115dbc9d7bb98056786f486ec0a38
Dave <dave.johnston@icloud.com>
2020-10-30 09:37:25 +0000

    (LIDAR-557) Retrive the fact type
    Add the fact-paths API which enables us to query the 'type' of a fact.

--- pkg/puppetdb/facts_test.go
+++ pkg/puppetdb/facts_test.go
@@ -15,6 +15,15 @@ func TestFactNames(t *testing.T) {
 	require.Equal(t, expectedFactNames, actual)
 }
 
+// TestFactPaths performs a test on the FactPaths endpoint and verifies the expected response is returned.
+func TestFactPaths(t *testing.T) {
+	// Test FactNames
+	setupGetResponder(t, factPaths, "", "factpaths-response.json")
+	actual, err := pdbClient.FactPaths("", nil, nil)
+	require.Nil(t, err)
+	require.Equal(t, expectedFactPaths, actual)
+}
+
 // TestFacts performs a test on the Facts method, and verified the expected response is returned,
 func TestFacts(t *testing.T) {
 	// Test with query
@@ -26,6 +35,7 @@ func TestFacts(t *testing.T) {
 }
 
 var expectedFactNames = []string{"agent_canary", "agent_specified_environment", "aio_agent_build", "aio_agent_version"}
+var expectedFactPaths = []FactPath{FactPath{Path: []string{"partitions", "sda3", "mount"}, Type: "string"}, FactPath{Path: []string{"partitions", "sda3", "size"}, Type: "string"}, FactPath{Path: []string{"partitions", "sda3", "uuid"}, Type: "string"}}
 var expectedFacts = []Fact{
 	{Name: "id", Value: "root", Certname: "foobar.puppetlabs.net", Environment: "production"},
 	{Name: "os", Value: map[string]interface{}{"architecture": "x86_64", "distro": map[string]interface{}{"codename": "Core", "description": "CentOS Linux release 7.4.1708 (Core)", "id": "CentOS", "release": map[string]interface{}{"full": "7.4.1708", "major": "7", "minor": "4"}, "specification": ":core-4.1-amd64:core-4.1-noarch"}, "family": "RedHat", "hardware": "x86_64", "name": "CentOS", "release": map[string]interface{}{"full": "7.4.1708", "major": "7", "minor": "4"}, "selinux": map[string]interface{}{"config_mode": "permissive", "config_policy": "targeted", "current_mode": "permissive", "enabled": true, "enforced": false, "policy_version": "28"}}, Certname: "foobar.puppetlabs.net", Environment: "production"},
