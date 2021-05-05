688225f09752b07da9c5c72133b37410aee9cca1
Luke Abernethy <luke.abernethy@puppet.com>
2021-05-05 10:19:10 +0100

    (CISC-1167) Create new error type for when warnings dont match orchErr

--- pkg/orch/utils.go
+++ pkg/orch/utils.go
@@ -1,11 +1,20 @@
 package orch
 
 import (
-	"fmt"
 	"strings"
 
 	"github.com/go-resty/resty/v2"
 )
+
+// UnmarshalError represents an error response from the Orchestrator Error Cast
+type UnmarshalError struct {
+	Msg        string
+	StatusCode int
+}
+
+func (oe *UnmarshalError) Error() string {
+	return oe.Msg
+}
 
 // FormatOrchError takes the resty response and a possible resty err and tries to create an
 // OrchestratorError with as much info as possible
@@ -14,7 +23,10 @@ func FormatOrchError(r *resty.Response, customError ...string) error {
 	orchErr, ok := r.Error().(*OrchestratorError)
 
 	if !ok {
-		return fmt.Errorf("unable to unmarshal an OrchestratorError, StatusCode: %s", r.Status())
+		return &UnmarshalError{
+			Msg:        "unable to unmarshal an OrchestratorError",
+			StatusCode: r.StatusCode(),
+		}
 	}
 
 	if len(customError) > 0 {
