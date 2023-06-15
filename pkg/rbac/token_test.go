90eb2c76d185ae72eb28f91cd80d0becd018421c
eoinmcq <eoin.mcquillan@puppet.com>
2023-06-15 17:18:31 +0100

    (MAINT) Add token authenticate endpoint

--- pkg/rbac/token_test.go
+++ pkg/rbac/token_test.go
@@ -28,3 +28,24 @@ func TestGetRBACToken(t *testing.T) {
 	require.Nil(t, actual)
 	require.Equal(t, expectedError, err)
 }
+
+func TestAuthenticateRBACToken(t *testing.T) {
+	// Test success
+	setupPostResponder(t, tokenAuthenticateURI, "AuthenticateRBACToken-request.json",
+		"AuthenticateRBACToken-response.json")
+
+	expectedResponse := &AuthenticateResponse{
+		UserID:      "abc",
+		Description: "abc",
+		RoleIDs:     []int{1, 2, 3},
+	}
+	actual, err := rbacClient.AuthenticateRBACToken("blah")
+	require.Nil(t, err)
+	require.Equal(t, expectedResponse, actual)
+
+	// Test error
+	setUpBadRequestResponder(t, http.MethodPost, tokenAuthenticateURI)
+	actual, err = rbacClient.AuthenticateRBACToken("blah")
+	require.Nil(t, actual)
+	require.Equal(t, expectedError, err)
+}
