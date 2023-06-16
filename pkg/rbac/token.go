c5ba452665234bb3d0ee44fec0109b9f12841ff6
eoinmcq <eoin.mcquillan@puppet.com>
2023-06-16 09:55:09 +0100

    (MAINT) Add token authenticate endpoint (#87)

--- pkg/rbac/token.go
+++ pkg/rbac/token.go
@@ -1,7 +1,8 @@
 package rbac
 
 const (
-	requestAuthTokenURI = "/rbac-api/v1/auth/token" // #nosec - this is the uri to get RBAC tokens
+	requestAuthTokenURI  = "/rbac-api/v1/auth/token"              // #nosec - this is the uri to g et RBAC tokens
+	tokenAuthenticateURI = "/rbac-api/v2/auth/token/authenticate" // #nosec - this is the uri to authenticate RBAC tokens
 )
 
 // GetRBACToken returns an auth token given user/password information
@@ -11,6 +12,27 @@ func (c *Client) GetRBACToken(authRequest *RequestKeys) (*Token, error) {
 		SetResult(&payload).
 		SetBody(authRequest).
 		Post(requestAuthTokenURI)
+	if err != nil {
+		return nil, FormatError(r, err.Error())
+	}
+	if r.IsError() {
+		if r.Error() != nil {
+			return nil, FormatError(r)
+		}
+		return nil, FormatError(r)
+	}
+	return &payload, nil
+}
+
+// AuthenticateRBACToken returns a response with the token details or errors otherwise.
+func (c *Client) AuthenticateRBACToken(token string) (*AuthenticateResponse, error) {
+	authenticateRequest := &AuthenticateRequest{Token: token}
+
+	payload := AuthenticateResponse{}
+	r, err := c.resty.R().
+		SetResult(&payload).
+		SetBody(authenticateRequest).
+		Post(tokenAuthenticateURI)
 	if err != nil {
 		return nil, FormatError(r, err.Error())
 	}
@@ -36,4 +58,19 @@ type RequestKeys struct {
 	Description string `json:"description,omitempty"`
 	Client      string `json:"client,omitempty"`
 	Label       string `json:"label,omitempty"`
+}
+
+// AuthenticateRequest will hold the request needed for an authenticate.
+type AuthenticateRequest struct {
+	Token              string `json:"token"`
+	UpdateLastActivity bool   `json:"update_last_activity?"`
+}
+
+// AuthenticateResponse will hold the response from an authenticate.
+type AuthenticateResponse struct {
+	Description string `json:"description"`
+	Login       string `json:"login"`
+	RoleIDs     []int  `json:"role_ids"`
+	UserID      string `json:"user_id"`
+	DisplayName string `json:"display_name"`
 }
